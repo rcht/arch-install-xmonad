859105ddbfa14ebf7c975e5e550270f031df77ec
Ermanno Ferrari <ermanno@ermannoferrari.net>
2022-03-07 19:54:29 +0000

    Update base-uefi.sh

--- base-uefi.sh
+++ base-uefi.sh
@@ -20,7 +20,8 @@ pacman -S grub efibootmgr networkmanager network-manager-applet dialog wpa_suppl
 # pacman -S --noconfirm xf86-video-amdgpu
 # pacman -S --noconfirm nvidia nvidia-utils nvidia-settings
 
-grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB
+grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB #change the directory to /boot/efi is you mounted the EFI partition at /boot/efi
+
 grub-mkconfig -o /boot/grub/grub.cfg
 
 systemctl enable NetworkManager
