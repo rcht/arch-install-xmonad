f45c636ffc88475cd994d576da06732b188f59fd
Rachit Arora <71589920+rcht@users.noreply.github.com>
2022-06-26 13:28:50 +0530

    Initial commit

--- base-uefi.sh
+++ base-uefi.sh
@@ -1,26 +1,43 @@
 #!/bin/bash
 
-ln -sf /usr/share/zoneinfo/Europe/Zurich /etc/localtime
+ln -sf /usr/share/zoneinfo/Asia/Kolkata /etc/localtime
 hwclock --systohc
 sed -i '178s/.//' /etc/locale.gen
 locale-gen
 echo "LANG=en_US.UTF-8" >> /etc/locale.conf
-echo "KEYMAP=de_CH-latin1" >> /etc/vconsole.conf
-echo "arch" >> /etc/hostname
+echo "archbtw" >> /etc/hostname
 echo "127.0.0.1 localhost" >> /etc/hosts
 echo "::1       localhost" >> /etc/hosts
-echo "127.0.1.1 arch.localdomain arch" >> /etc/hosts
+echo "127.0.1.1 archbtw.localdomain archbtw" >> /etc/hosts
 echo root:password | chpasswd
 
-# You can add xorg to the installation packages, I usually add it at the DE or WM install script
-# You can remove the tlp package if you are installing on a desktop or vm
+# base packages
+pacman -S --noconfirm grub efibootmgr networkmanager network-manager-applet dialog wpa_supplicant \
+          mtools dosfstools base-devel linux-headers avahi xdg-user-dirs xdg-utils gvfs gvfs-smb nfs-utils inetutils dnsutils \
+          bluez bluez-utils cups hplip alsa-utils pipewire pipewire-alsa pipewire-pulse pipewire-jack bash-completion openssh \ 
+          rsync reflector acpi acpi_call tlp virt-manager edk2-ovmf bridge-utils dnsmasq vde2 openbsd-netcat \
+          iptables-nft ipset firewalld flatpak sof-firmware nss-mdns acpid os-prober ntfs-3g terminus-font \
+          qemu-base qemu-desktop qemu-full qemu-emulators-full 
 
-pacman -S grub efibootmgr networkmanager network-manager-applet dialog wpa_supplicant mtools dosfstools base-devel linux-headers avahi xdg-user-dirs xdg-utils gvfs gvfs-smb nfs-utils inetutils dnsutils bluez bluez-utils cups hplip alsa-utils pipewire pipewire-alsa pipewire-pulse pipewire-jack bash-completion openssh rsync reflector acpi acpi_call tlp virt-manager qemu qemu-arch-extra edk2-ovmf bridge-utils dnsmasq vde2 openbsd-netcat iptables-nft ipset firewalld flatpak sof-firmware nss-mdns acpid os-prober ntfs-3g terminus-font
+# graphics and touchpad drivers
+pacman -S --noconfirm xf86-video-intel xf86-input-libinput
 
-# pacman -S --noconfirm xf86-video-amdgpu
-# pacman -S --noconfirm nvidia nvidia-utils nvidia-settings
+# GUI applications
+pacman -S --noconfirm firefox libreoffice yad alacritty yakuake konsole retroarch libretro zathura zathura-djvu zathura-pdf-poppler sxiv pcmanfm nitrogen feh obs-studio
 
-grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB #change the directory to /boot/efi is you mounted the EFI partition at /boot/efi
+# stuff made in rust :)
+pacman -S --noconfirm fd bat exa fzf
+
+# terminal stuff
+pacman -S --noconfirm asciinema neofetch cmus newsboat
+
+# fonts
+pacman -S --noconfirm ttf-fira-code ttf-roboto-mono
+
+# other stuff
+pacman -S --noconfirm rofi rofi-calc xmobar
+
+grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB 
 
 grub-mkconfig -o /boot/grub/grub.cfg
 
@@ -29,22 +46,18 @@ systemctl enable bluetooth
 systemctl enable cups.service
 systemctl enable sshd
 systemctl enable avahi-daemon
-systemctl enable tlp # You can comment this command out if you didn't install tlp, see above
+systemctl enable tlp 
 systemctl enable reflector.timer
 systemctl enable fstrim.timer
 systemctl enable libvirtd
 systemctl enable firewalld
 systemctl enable acpid
 
-useradd -m ermanno
-echo ermanno:password | chpasswd
-usermod -aG libvirt ermanno
+useradd -m rachit
+echo rachit:rcht | chpasswd
+usermod -aG libvirt rachit
 
-echo "ermanno ALL=(ALL) ALL" >> /etc/sudoers.d/ermanno
+echo "rachit ALL=(ALL) ALL" >> /etc/sudoers.d/rachit
 
-
-printf "\e[1;32mDone! Type exit, umount -a and reboot.\e[0m"
-
-
-
-
+clear
+printf "\e[1;32mNote to self: Make sure to install rofi-bluetooth and the rest of your stuff later.\e[0m\n"
